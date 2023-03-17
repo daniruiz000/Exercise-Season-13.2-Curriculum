d219a491c7df5953c1c9eed93b55acf620c9bd96
Daniel Ruiz <danielruimar@gmail.com>
2023-03-17 18:48:06 +0100

    change direction

--- src/components/Knowledge/Knowledge.js
+++ src/components/Knowledge/Knowledge.js
@@ -15,7 +15,7 @@ const dataInfo =[
         title:'React.js',
         text:'React es una biblioteca de JS de código abierto diseñada para crear interfaces de usuario con el onjetivo de facilitar el desarrollo de aplicaciones en una sola página.',
         imageUrl:'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
-        url:'https://es.reactjs.org/'
+        url:'https://react.dev/'
     },
     {
         id:2,
