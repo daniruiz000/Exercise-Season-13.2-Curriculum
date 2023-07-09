ba97b9b0a3f43230ab68290d167a31c06dd0a8b8
Daniel Ruiz <danielruimar@gmail.com>
2023-07-09 21:56:08 +0200

    Added more proyects

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,47 @@
-import './Header.css'
-import { Text, Image, Flex, Spacer} from '@chakra-ui/react'
-
+import "./Header.css";
+import { Text, Image, Flex, Spacer } from "@chakra-ui/react";
 
 const Header = () => {
+  return (
+    <Flex
+      margin="40px auto"
+      boxShadow="5px 5px 5px 5px grey"
+      as="div"
+      w={700}
+      h={250}
+      color="white"
+      background="#3182CF"
+      borderRadius={50}
+      justify="center"
+      align="center"
+      direction="row"
+    >
+      <Spacer />
 
-    return (
-        
-        <Flex
-            margin='40px auto'
-            boxShadow='5px 5px 5px 5px grey'
-            as='div'
-            w={700}
-            h={250}
-            color='white'
-            background='#3182CF'
-            borderRadius={50}
-            justify='center'
-            align='center'
-            direction='row'>
+      <Flex direction="column" justify="center" align="center">
+        <Text fontSize={30} fontWeight={800}>
+          Daniel Ruiz Maroto
+        </Text>
+        <Text fontSize={20} fontWeight={300}>
+          Desarrollador Web Full Stack
+        </Text>
+      </Flex>
 
-            <Spacer />
+      <Spacer />
 
-            <Flex direction='column'
-                justify='center'
-                align='center'>
-                <Text fontSize={30} fontWeight={800}>Daniel Ruiz Maroto</Text>
-                <Text fontSize={20} fontWeight={300}>Desarrollador Web Full Stack</Text>
-            </Flex>
+      <Image
+        boxSize="300px"
+        objectFit="cover"
+        borderRadius={50}
+        w={170}
+        h={200}
+        src={process.env.PUBLIC_URL + "/1516641943147.jpg"}
+        alt="Captura de pantalla"
+      />
 
-            <Spacer />
-
-            <Image
-                boxSize='300px'
-                objectFit='cover'
-                borderRadius={50}
-                w={170}
-                h={200}
-                alt='portada'
-                src='https://scontent-mad2-1.xx.fbcdn.net/v/t1.18169-9/16105532_10211778276602180_9130556473616655902_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=MqHj5VrqiLAAX-eCX8p&_nc_ht=scontent-mad2-1.xx&oh=00_AfBqFL3Wl7Z4hHTViuxbG5Y9ghdkAbj1UiiNR0GWHnfsiw&oe=643A9B9D'
-                />
-            
-            <Spacer />
-            
-        </Flex>
-    )
-}
+      <Spacer />
+    </Flex>
+  );
+};
 
 export default Header;
--- src/components/Proyect/Proyect.js
+++ src/components/Proyect/Proyect.js
@@ -1,57 +1,57 @@
 import {
-    Image,
-    Box,
-    Button,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-} from '@chakra-ui/react'
-import React from 'react'
+  Image,
+  Box,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
 
 const Proyect = ({ project }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const initialRef = React.useRef(null)
-    const finalRef = React.useRef(null)
-
-    return (
-
-        <Box className='proyect'>
-            <Image
-                onClick={onOpen}
-                src={project.urlImg}
-                alt='Rick and Morty'
-                w={'300PX'}
-                h={'200px'}
-                borderRadius={20}
-                margin={5}/>
-            <Modal
-                isOpen={isOpen}
-                onClose={onClose}
-                initialFocusRef={initialRef}
-                finalFocusRef={finalRef}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>{project.title}</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody ref={initialRef}>
-                        {project.text}
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={onClose}>
-                            Cerrar
-                        </Button>
-                        <Button variant='ghost' onClick={() => window.open(project.url)}>Ir a la App</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </Box>
-    )
-}
+  return (
+    <Box className="proyect">
+      <Image
+        onClick={onOpen}
+        src={project.urlImg}
+        alt="app"
+        w={"300PX"}
+        h={"200px"}
+        borderRadius={20}
+        margin={5}
+      />
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{project.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody ref={initialRef}>{project.text}</ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Cerrar
+            </Button>
+            <Button variant="ghost" onClick={() => window.open(project.url)}>
+              Ir a la App
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
+};
 
 export default Proyect;
