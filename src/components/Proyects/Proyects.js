9e0809372833260fe3c48d178aaf6920bd838436
Daniel Ruiz <danielruimar@gmail.com>
2023-08-21 13:48:31 +0200

    Added pokemon app

--- src/components/Proyects/Proyects.js
+++ src/components/Proyects/Proyects.js
@@ -7,34 +7,34 @@ const dataProject = [
   {
     id: 0,
     urlImg:
-      "https://github.com/daniruiz000/RICK-MORTY/blob/main/RickMorty.png?raw=true",
-    title: "Rick and Morty",
-    text: "Wiki de la famosa serie realizada en JS plano, con SASS y Metodolgía BEN.",
-    url: "https://daniruiz000.github.io/RICK-MORTY/",
-  },
-  {
-    id: 1,
-    urlImg:
       "https://github.com/daniruiz000/Chuck-Norris/blob/main/Chuck.png?raw=true",
     title: "Chuk Norris Jokes",
     text: "App que nos devolverá chistes en funcion de la tematica realizada en JS plano, con SASS y Metodolgía BEN.",
     url: "https://daniruiz000.github.io/Chuck-Norris/",
   },
   {
-    id: 2,
+    id: 1,
     urlImg:
       "https://github.com/daniruiz000/Adivina-tu-edad/blob/main/Adivina.png?raw=true",
     title: "Adivina tu edad",
-    text: " App que adivinará nuestra edad en funcion del nombre que introduzcamos realizada en JS plano, con SASS y Metodolgía BEN.",
+    text: " App que adivinará nuestra edad en función del nombre que introduzcamos realizada en JS plano, con SASS y Metodolgía BEN.",
     url: "https://daniruiz000.github.io/Adivina-tu-edad/",
   },
   {
-    id: 3,
+    id: 2,
     urlImg:
       "https://github.com/daniruiz000/Ejercicio-Sesion-5.2-Calculadora/blob/main/Captura%20de%20pantalla%202023-03-16%20221139.png?raw=true",
     title: "Calculadora",
     text: "App calculadora básica realizada en JS plano, con SASS y Metodolgía BEN.",
     url: "https://daniruiz000.github.io/Ejercicio-Sesion-5.2-Calculadora/",
+  },
+  {
+    id: 3,
+    urlImg:
+      "https://github.com/daniruiz000/RICK-MORTY/blob/main/RickMorty.png?raw=true",
+    title: "Rick and Morty",
+    text: "Wiki de la famosa serie realizada en JS plano, con SASS y Metodolgía BEN.",
+    url: "https://daniruiz000.github.io/RICK-MORTY/",
   },
   {
     id: 4,
@@ -44,12 +44,19 @@ const dataProject = [
     url: "https://laliga-front.onrender.com",
   },
   {
-    id: 4,
+    id: 5,
     urlImg:
       process.env.PUBLIC_URL + "/Captura de pantalla 2023-07-09 215438.png",
     title: "TMDB-FILMS",
     text: "App que emula la famosa página de cine y tv de TMDB y hace uso de su API pública. API creada con Node. además incluye un juego propio implentado por mí.",
     url: "https://bootcam-films.onrender.com/",
+  },
+  {
+    id: 6,
+    urlImg: process.env.PUBLIC_URL + "/Pokemon.png",
+    title: "POKEMON",
+    text: "App que hace uso de la API pública de Pokemon, además incluye un juego propio implentado por mí.",
+    url: "https://pokemon-daniel-ruiz.onrender.com",
   },
 ];
 
@@ -68,9 +75,11 @@ const Proyects = () => {
         Estos son algunos de mis mejores trabajos:
       </Text>
       <Flex justify={"center"} wrap={"wrap"} margin={"0 auto"}>
-        {dataProject.map((project) => (
-          <Proyect project={project} />
-        ))}
+        {dataProject
+          .sort((a, b) => b.id - a.id)
+          .map((project) => (
+            <Proyect project={project} />
+          ))}
       </Flex>
     </Flex>
   );
