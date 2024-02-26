a6404eb06e804e644c240ccf21d53334a13e5d06
Daniel Ruiz <danielruimar@gmail.com>
2024-02-26 11:52:40 +0100

    b

--- src/components/Knowledge/Knowledge.js
+++ src/components/Knowledge/Knowledge.js
@@ -1,70 +1,39 @@
-import './Knowledge.css'
-import ImageCard from '../ImageCard/ImageCard'
+import './Knowledge.css';
+import ImageCard from '../ImageCard/ImageCard';
 import { Flex, Text } from '@chakra-ui/react';
-
-const dataInfo =[
-    {
-        id:0,
-        title:'Node.js',
-        text:'Node.js es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor basado en el lenguaje de programación JavaScript',
-        imageUrl:'https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.png',
-        url:'https://nodejs.org/es'
-    },
-    {
-        id:1,
-        title:'React.js',
-        text:'React es una biblioteca de JS de código abierto diseñada para crear interfaces de usuario con el onjetivo de facilitar el desarrollo de aplicaciones en una sola página.',
-        imageUrl:'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
-        url:'https://react.dev/'
-    },
-    {
-        id:2,
-        title:'HTML5',
-        text:'HTML 5  es la quinta revisión importante del lenguaje básico HTML.',
-        imageUrl:'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
-        url:''
-    },
-    {
-        id:3,
-        title:'CSS',
-        text:'CSS es el lenguaje de estilos utilizado para describir la presentación de documentos HTML o XML (en-US) (incluyendo varios lenguajes basados en XML como SVG, MathML o XHTML).',
-        imageUrl:'https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-css-logo-png-transparent-svg-vector-bie-supply-9.png',
-        url:'https://developer.mozilla.org/es/docs/Web/CSS'
-    }
-]
-
+import { dataInfo } from '../../data';
 
 const Knowledge = () => {
-
-    return (
-        <Flex
-            direction={'column'}
-            boxShadow='5px 5px 5px 5px grey'
-            color='white'
-            width={700}
-            background='#3182CF'
-            margin={'50px auto'}
-            borderRadius={50}>
-            <Text
-                margin={'20px auto'}
-                fontSize={30}
-                fontWeight={600}
-            >Estos son algunos de mis conocimientos:
-            </Text>
-            <Flex
-                direction={'row'}
-                borderRadius={50}
-                wrap='wrap'
-                padding={10}
-                color='white'
-                width={700}
-                background='#3182CF'
-                margin={'0 auto'}
-                justify={'center'}
-            >{dataInfo.map(element=> <ImageCard key={element.id} imageUrl={element.imageUrl} text={element.text} title={element.title} url={element.url}/>)}
-            </Flex>
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      direction={'column'}
+      boxShadow="5px 5px 5px 5px grey"
+      color="white"
+      width={700}
+      background="#3182CF"
+      margin={'50px auto'}
+      borderRadius={50}
+    >
+      <Text margin={'20px auto'} fontSize={30} fontWeight={600}>
+        Estos son algunos de mis conocimientos:
+      </Text>
+      <Flex
+        direction={'row'}
+        borderRadius={50}
+        wrap="wrap"
+        padding={10}
+        color="white"
+        width={700}
+        background="#3182CF"
+        margin={'0 auto'}
+        justify={'center'}
+      >
+        {dataInfo.map((element) => (
+          <ImageCard key={element.id} imageUrl={element.imageUrl} text={element.text} title={element.title} url={element.url} />
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
 
 export default Knowledge;
---
+++ src/data.js
@@ -0,0 +1,105 @@
+export const dataProject = [
+  {
+    id: 0,
+    urlImg: 'https://github.com/daniruiz000/Chuck-Norris/blob/main/Chuck.png?raw=true',
+    title: 'Chuk Norris Jokes',
+    text: 'App que nos devolverá chistes en funcion de la tematica realizada en JS plano, con SASS y Metodolgía BEN.',
+    url: 'https://daniruiz000.github.io/Chuck-Norris/',
+  },
+  {
+    id: 1,
+    urlImg: 'https://github.com/daniruiz000/Adivina-tu-edad/blob/main/Adivina.png?raw=true',
+    title: 'Adivina tu edad',
+    text: ' App que adivinará nuestra edad en función del nombre que introduzcamos realizada en JS plano, con SASS y Metodolgía BEN.',
+    url: 'https://daniruiz000.github.io/Adivina-tu-edad/',
+  },
+  {
+    id: 2,
+    urlImg:
+      'https://github.com/daniruiz000/Ejercicio-Sesion-5.2-Calculadora/blob/main/Captura%20de%20pantalla%202023-03-16%20221139.png?raw=true',
+    title: 'Calculadora',
+    text: 'App calculadora básica realizada en JS plano, con SASS y Metodolgía BEN.',
+    url: 'https://daniruiz000.github.io/Ejercicio-Sesion-5.2-Calculadora/',
+  },
+  {
+    id: 3,
+    urlImg: process.env.PUBLIC_URL + '/starwars.png',
+    title: 'STARWARS',
+    text: 'Wiki de la saga de STAR WARS realizada en JS plano, con SASS y Metodolgía BEN.',
+    url: 'https://daniruiz000.github.io/Star-Wars/',
+  },
+  {
+    id: 4,
+    urlImg: 'https://github.com/daniruiz000/RICK-MORTY/blob/main/RickMorty.png?raw=true',
+    title: 'Rick and Morty',
+    text: 'Wiki de la famosa serie realizada en JS plano, con SASS y Metodolgía BEN.',
+    url: 'https://daniruiz000.github.io/RICK-MORTY/',
+  },
+  {
+    id: 5,
+    urlImg: process.env.PUBLIC_URL + '/laliga.png',
+    title: 'LaLiga',
+    text: 'App para la gestión y creación de ligas de equipos de fútbol. API creada con Node.',
+    url: 'https://laliga-front.onrender.com',
+  },
+  {
+    id: 6,
+    urlImg: process.env.PUBLIC_URL + '/Captura de pantalla 2023-07-09 215438.png',
+    title: 'TMDB-FILMS',
+    text: 'App que emula la famosa página de cine y tv de TMDB y hace uso de su API pública. API creada con Node. además incluye un juego propio implentado por mí.',
+    url: 'https://bootcam-films.onrender.com/',
+  },
+  {
+    id: 7,
+    urlImg: process.env.PUBLIC_URL + '/Pokemon.png',
+    title: 'POKEMON',
+    text: 'App que hace uso de la API pública de Pokemon, además incluye un juego propio implentado por mí.',
+    url: 'https://pokemon-daniel-ruiz.onrender.com',
+  },
+  {
+    id: 8,
+    urlImg: process.env.PUBLIC_URL + '/Landing.png',
+    title: 'Landing Halloween',
+    text: 'Landing promocional con temática de Halloween, frontal realizado con React en el que los usuarios pueden registrarse en una promoción. La gestión de la promoción la he realizado mediante un Back basado en Node y Express con todo tipo de validaciones.',
+    url: 'https://landing-prueba-front.onrender.com',
+  },
+  {
+    id: 9,
+    urlImg: process.env.PUBLIC_URL + '/Carmen.png',
+    title: 'Carmen Maroto Art',
+    text: 'Página realizada con React para promoción de la mejor pintora del mundo... mi madre.',
+    url: 'https://carmen-maroto-art.onrender.com',
+  },
+];
+
+export const dataInfo = [
+  {
+    id: 0,
+    title: 'Node.js',
+    text: 'Node.js es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor basado en el lenguaje de programación JavaScript',
+    imageUrl: 'https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.png',
+    url: 'https://nodejs.org/es',
+  },
+  {
+    id: 1,
+    title: 'React.js',
+    text: 'React es una biblioteca de JS de código abierto diseñada para crear interfaces de usuario con el onjetivo de facilitar el desarrollo de aplicaciones en una sola página.',
+    imageUrl:
+      'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
+    url: 'https://react.dev/',
+  },
+  {
+    id: 2,
+    title: 'HTML5',
+    text: 'HTML 5  es la quinta revisión importante del lenguaje básico HTML.',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+    url: '',
+  },
+  {
+    id: 3,
+    title: 'CSS',
+    text: 'CSS es el lenguaje de estilos utilizado para describir la presentación de documentos HTML o XML (en-US) (incluyendo varios lenguajes basados en XML como SVG, MathML o XHTML).',
+    imageUrl: 'https://www.freepnglogos.com/uploads/html5-logo-png/html5-logo-css-logo-png-transparent-svg-vector-bie-supply-9.png',
+    url: 'https://developer.mozilla.org/es/docs/Web/CSS',
+  },
+];
